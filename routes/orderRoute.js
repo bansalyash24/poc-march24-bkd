afa59b2909d53363b88c3038cce18107d438b633
bansalyash24 <bansalyash066@gmail.com>
2024-03-07 14:02:58 +0530

    feat:positions route

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -29,5 +29,32 @@ router.post('/orders',async(req,res)=>{
     
 })
 
+router.get('/positions',async(req,res)=>{
+    console.log(req.headers["access-token"],req.body)
+    try{
+        const dhanResp=await fetch("https://api.dhan.co/positions",{
+            method:"GET",
+            headers:{
+                "access-token": req.headers["access-token"],
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+        })
+        const dhanData=await dhanResp.json()
+        res.json({
+            data:dhanData,
+            message:"Positions fetched",
+            success:true
+        })
+    }catch(err){
+        res.status(500).json({
+            data:null,
+            message:err.message,
+            success:false
+        })
+    }
+    
+})
+
 
 module.exports=router
